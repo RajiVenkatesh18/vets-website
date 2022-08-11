91433f48777029c4366351dfabef6dac0d439d8c
Darius Dzien <dariusz.dzien@adhocteam.us>
2022-08-10 19:08:12 -0500

    WIP

--- src/applications/coronavirus-vaccination/tests/e2e/signup.cypress.spec.js
+++ src/applications/coronavirus-vaccination/tests/e2e/signup.cypress.spec.js
@@ -9,7 +9,7 @@ describe('COVID-19 Vaccination Preparation Form', () => {
       );
     });
 
-    it('should successfully submit the vaccine preparation form test', () => {
+    it('should successfully submit the vaccine preparation form', () => {
       const stayInformedPage = new StayInformedPage();
 
       // Intro page
