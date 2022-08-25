c6a3456d23ba412312321ae1ac02e486f8db68e0
Rodriguez Renteria <ROBERTO.RODRIGUEZRENTERIA@va.gov>
2022-08-25 12:29:23 -0400

    [#MHV-33797] changed hamburger menu icon from an svg to a font awesome icon

--- src/applications/mhv/secure-messaging/components/Navigation.jsx
+++ src/applications/mhv/secure-messaging/components/Navigation.jsx
@@ -30,14 +30,7 @@ const Navigation = () => {
             onClick={openNavigation}
           >
             <strong>In the Messages section</strong>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="16"
-              viewBox="0 0 448 512"
-            >
-              <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
-            </svg>
+            <i className="fas fa-bars" />
           </button>
         </div>
       </div>
