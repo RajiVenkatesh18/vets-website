a536c2b0d3a6a2073b132a2bca9cd49e1631bec2
Rodriguez Renteria <ROBERTO.RODRIGUEZRENTERIA@va.gov>
2022-08-31 10:54:08 -0400

    [MHV-33535] temporarily combined both folder and messaging reducers until combineReducers redux function is working

--- src/applications/mhv/secure-messaging/components/MessageActionsButtons.jsx
+++ src/applications/mhv/secure-messaging/components/MessageActionsButtons.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { VaModal } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { fetchAllFolders } from '../actions/index';
 
 const MessageActionButtons = () => {
+  const dispatch = useDispatch();
+  const { isLoading, folders, error } = useSelector(
+    state => state?.allMessages,
+  );
+
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [moveToFolder, setMoveToFolder] = useState('');
 
-  const folderList = [
-    {
-      name: 'Deleted Messages',
+  useEffect(
+    () => {
+      dispatch(fetchAllFolders());
     },
-    {
-      name: 'Folder 1',
-    },
-    {
-      name: 'Folder 2',
-    },
-    {
-      name: 'Folder 3',
-    },
-  ];
+    [dispatch],
+  );
 
   const openMoveModal = () => {
     setIsModalVisible(true);
@@ -33,7 +32,52 @@ const MessageActionButtons = () => {
   };
 
   const handleConfirmMoveFolderTo = () => {
+    // dispatch(moveMessageToFolder());
     setIsModalVisible(false);
+  };
+
+  const moveVaModal = () => {
+    return isModalVisible ? (
+      <div className="message-actions-buttons-modal">
+        <VaModal
+          id="move-to-modal"
+          large
+          modalTitle="Move to:"
+          onCloseEvent={closeMoveModal}
+          onPrimaryButtonClick={handleConfirmMoveFolderTo}
+          onSecondaryButtonClick={closeMoveModal}
+          primaryButtonText="Confirm"
+          secondaryButtonText="Cancel"
+          visible={isModalVisible}
+        >
+          <div className="modal-body">
+            <p>
+              This conversation will be moved. Any replies to this message will
+              appear in your inbox
+            </p>
+            {folders.folder.map(folder => (
+              <div key={folder.name} className="form-radio-buttons">
+                <div className="radio-button">
+                  <input
+                    type="radio"
+                    autoComplete="false"
+                    id="defaultId-0"
+                    name="defaultName"
+                    value={folder.name}
+                    onChange={handleOnChangeFolder}
+                  />
+                  <label name="defaultName-0-label" htmlFor="defaultId-0">
+                    {folder.name}
+                  </label>
+                </div>
+              </div>
+            ))}
+            {isLoading ? <p>Moving Message to: {moveToFolder}</p> : null}
+            {error ? <p>{error}</p> : null}
+          </div>
+        </VaModal>
+      </div>
+    ) : null;
   };
 
   return (
@@ -56,46 +100,7 @@ const MessageActionButtons = () => {
         <i className="fas fa-folder" />
         <span className="message-action-button-text">Move</span>
       </button>
-      {isModalVisible ? (
-        <div className="message-actions-buttons-modal">
-          <VaModal
-            id="move-to-modal"
-            large
-            modalTitle="Move to:"
-            onCloseEvent={closeMoveModal}
-            onPrimaryButtonClick={handleConfirmMoveFolderTo}
-            onSecondaryButtonClick={closeMoveModal}
-            primaryButtonText="Confirm"
-            secondaryButtonText="Cancel"
-            visible={isModalVisible}
-          >
-            <div className="modal-body">
-              <p>
-                This conversation will be moved. Any replies to this message
-                will appear in your inbox
-              </p>
-              {folderList.map(folder => (
-                <div key={folder.name} className="form-radio-buttons">
-                  <div className="radio-button">
-                    <input
-                      type="radio"
-                      autoComplete="false"
-                      id="defaultId-0"
-                      name="defaultName"
-                      value={folder.name}
-                      onChange={handleOnChangeFolder}
-                    />
-                    <label name="defaultName-0-label" htmlFor="defaultId-0">
-                      {folder.name}
-                    </label>
-                  </div>
-                </div>
-              ))}
-              <p>Moving Message to: {moveToFolder}</p>
-            </div>
-          </VaModal>
-        </div>
-      ) : null}
+      {moveVaModal()}
       <button type="button" className="message-action-button">
         <i className="fas fa-reply" />
         <span className="message-action-button-text">Reply</span>
--- src/applications/mhv/secure-messaging/reducers/index.js
+++ src/applications/mhv/secure-messaging/reducers/index.js
@@ -1,8 +1,60 @@
-import { combineReducers } from 'redux';
+import {
+  MESSAGES_RETRIEVE_STARTED,
+  MESSAGES_RETRIEVE_SUCCEEDED,
+  MESSAGES_RETRIEVE_FAILED,
+  FETCH_ALL_FOLDERS_STARTED,
+  FETCH_ALL_FOLDERS_FAILED,
+  FETCH_ALL_FOLDERS_SUCCEEDED,
+} from '../actions';
 
-import allMessages from './messagesReducer';
-import allFolders from './foldersReducer';
-
-export default {
-  secureMessaging: combineReducers({ allMessages, allFolders }),
+const initialState = {
+  isLoading: true,
+  messages: null,
+  folders: null,
+  error: null,
 };
+
+const allMessages = (state = initialState, action) => {
+  switch (action.type) {
+    case MESSAGES_RETRIEVE_STARTED:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case MESSAGES_RETRIEVE_SUCCEEDED:
+      return {
+        ...state,
+        isLoading: false,
+        messages: action.response,
+      };
+    case MESSAGES_RETRIEVE_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.response,
+      };
+    case FETCH_ALL_FOLDERS_STARTED:
+      return {
+        ...state,
+        isLoading: true,
+        error: '',
+      };
+    case FETCH_ALL_FOLDERS_SUCCEEDED:
+      return {
+        ...state,
+        isLoading: false,
+        folders: action.payload,
+        error: '',
+      };
+    case FETCH_ALL_FOLDERS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        folders: '',
+        error: action.response,
+      };
+    default:
+      return state;
+  }
+};
+export default { allMessages };
